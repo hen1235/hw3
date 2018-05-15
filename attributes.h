4e0e41fc5137fd57fc192185c8b9326b8572cde5
Hen Leibovich <hen1235.hl@gmail.com>
2018-05-15 10:26:39 -0700

    finished all stages before comment 4

---
+++ attributes.h
@@ -0,0 +1,42 @@
+#ifndef __ATTRIBUTES_H
+#define __ATTRIBUTES_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "output.hpp"
+
+//#define YYSTYPE STYPE	
+
+using namespace std;
+using namespace output;
+
+typedef enum Type
+{
+	_ID,
+	_BOOLEAN,
+	_NUM,
+	_STRING,
+	_B,
+	_VOID
+} type_t;
+
+typedef struct Exp {
+	type_t type;
+	union {
+		int val;
+		bool b;
+		//string str;
+	}; 
+
+} exp_t;
+
+
+
+
+
+
+
+
+
+#endif
